69ae6cd8392d17a41e9e00792f822d62feac1719
jossiesteven <jossiesteven@gmail.com>
2023-06-08 14:42:11 -0500

    Initial commit

---
+++ routes/api.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+
+/*
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+*/
+
+Route::get("/saludo",function(Request $request){
+    
+    $message=['mensaje'=>"Hola Mundo!"];
+    return response()->json($message);
+});
+
+Route::post("/libro",function(Request $request){
+    
+    $message=['book'=>"Cien años de soledad"];
+    return response()->json($message);
+});
+
+Route::put("/carro",function(Request $request){
+    
+    $message = ['car'=>"Toyota Land Cruiser",
+    'disenio'=> [
+        'color'=>'rojo',
+        'tamanio'=>'amplio',
+        'detalle'=>[1,2,3,4,5]
+    ]];
+
+    return response()->json($message);
+});
+
+Route::patch("/computador",function(Request $request){
+    
+    $message=['computer'=>"Compaq"];
+    return response()->json($message);
+});
+
+Route::delete("/motocicleta",function(Request $request){
+    
+    $message=['motorbike'=>"Honda Bross 125"];
+    return response()->json($message);
+});
